b71117c752fa512316116d5d9503776471969121
Gaurav140611 <kumarsinghgaurav54@gmail.com>
2021-10-14 14:00:45 +0530

    bug test

--- spam_results.py
+++ spam_results.py
@@ -13,45 +13,45 @@ from datetime import datetime
 from elasticsearch import Elasticsearch
 from random import choice
 
-def spam_results(content = None, num_features = 10):
+
+def spam_results(content=None, num_features=10):
     """
     content: string
     Article to be passed in the model
     default: None
-    
+
     num_features: int
     total words to consider for weights
     default: 10
     """
-    
+
     pipeline = joblib.load('tfidf_rfc.pkl')
-    
-    class_names=['0','1']
-    
+
+    class_names = ['0', '1']
+
     """
     class 0 -> NOT SPAM
     class 1 -> SPAM
     """
-    
+
     explainer = LimeTextExplainer(class_names=class_names)
-    exp = explainer.explain_instance(content, pipeline.predict_proba, num_features = num_features)
-    
+    exp = explainer.explain_instance(
+        content, pipeline.predict_proba, num_features=num_features)
+
     probability = pipeline.predict_proba([content])
-    
+
     weights = exp.as_list()
     weights_list = []
-    
+
     for weight in weights:
-        weights_list.append({"word":weight[0], "score": weight[1]})
-        
-        
-        
+        weights_list.append({"word": weight[0], "score": weight[1]})
+
     results = {}
-    
+
     results['weights'] = weights_list
-    
-    results['probability'] = [probability[0][0],probability[0][1]]
-    
+
+    results['probability'] = [probability[0][0], probability[0][1]]
+
     if probability[0][0] > probability[0][1]:
         results['predicted_class'] = '0'
     else:
@@ -59,9 +59,8 @@ def spam_results(content = None, num_features = 10):
     return results
 
 
-
 def get_data_from_db(db):
-    host = 'http://15.207.24.247:9200/' + db +'/_search'
+    host = 'http://15.207.24.247:9200/' + db + '/_search'
     json_body = '''{
         "query": {
                 "bool": {
@@ -77,63 +76,65 @@ def get_data_from_db(db):
         'Content-Type': 'application/json',
     }
     params = {
-        'size':100
+        'size': 100
     }
-    resp = requests.get(host,params=params, headers=headers, data=json_body)
+    resp = requests.get(host, params=params, headers=headers, data=json_body)
     resp_text = json.loads(resp.text)
     document_list = []
 
     for data in resp_text['hits']['hits']:
         try:
-                content_list = {}
-                content_list['id'] = data['_id']
-                content_list['content'] = data['_source']['Content']
-                document_list.append(content_list)
+            content_list = {}
+            content_list['id'] = data['_id']
+            content_list['content'] = data['_source']['Content']
+            document_list.append(content_list)
         except Exception as err:
-                print(err)
+            print(err)
 
     return document_list
 
-def writeInDB(elastic_client , doc_id , doc , db):
-    source_to_update = { "doc" : { "fake_news2" : doc } }
+
+def writeInDB(elastic_client, doc_id, doc, db):
+    source_to_update = {"doc": {"fake_news2": doc}}
     response = elastic_client.update(
         index=db,
         doc_type="_doc",
         id=doc_id,
         body=source_to_update
-        )
-    print(datetime.now().strftime("%d/%m/%Y %H:%M:%S"),response['result'])
+    )
+    print(datetime.now().strftime("%d/%m/%Y %H:%M:%S"), response['result'])
     if response['result'] == "updated":
         pass
         # print ("result:", response['result'])
         # print ("Update was a success for ID:", response['_id'])
         # print ("New data:", source_to_update)
     else:
-        print ("result:", response['result'])
-        print ("Response failed:", response['_shards']['failed'])
+        print("result:", response['result'])
+        print("Response failed:", response['_shards']['failed'])
 
     return response
 
-def write_data_in_db(id,result, db):
+
+def write_data_in_db(id, result, db):
     host = 'http://15.207.24.247:9200/'+db+'/_update/'+str(id)
     headers = {
         'Content-Type': 'application/json',
     }
     post_body = {
-        "doc" : {
-        "fake_news1" : result
-    },
-    "detect_noop": False
+        "doc": {
+            "fake_news1": result
+        },
+        "detect_noop": False
     }
 
     post_body = json.dumps(post_body)
-    response = requests.post(url=host,headers=headers,data=post_body)
+    response = requests.post(url=host, headers=headers, data=post_body)
     return response
 
 
 while True:
     elastic_client = Elasticsearch([{'host': '15.207.24.247', 'port': 9200}])
-    db = choice(['news_list','news_data'])
+    db = choice(['news_list', 'news_data'])
     data = get_data_from_db(db)
     # print(data)
 
@@ -142,20 +143,15 @@ while True:
     for ele in tqdm(data):
         text = ele['content']
         ans = spam_results(text)
-        ans = ans['weights']
-        ans = [
-            {
-                "word" : "money",
-                "score" : 0.233445435,
-            }
-        ]
-        print('->',[])
-        res = writeInDB(elastic_client , ele['id'],ans, db)
-        res = write_data_in_db(ele['id'], ans, db)
 
-        print(res)
-        cnt=cnt+1
-        break
+        print('->', ans)
+
+        # res = writeInDB(elastic_client , ele['id'],ans, db)
+        # res = write_data_in_db(ele['id'], ans, db)
+
+       # print(res)
+        cnt = cnt+1
+       # break
 
     print("*"*40)
     print(time.time()-start)
